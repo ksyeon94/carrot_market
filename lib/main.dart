842af819677c8f0169ff63b55ad63e6164512a03
seungyeon <rlatmddus159@naver.com>
2023-06-02 15:25:34 +0900

    init

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(myapp());
+
+class myapp extends StatefulWidget {
+  const myapp({Key? key}) : super(key: key);
+
+  @override
+  State<myapp> createState() => _myappState();
+}
+
+class _myappState extends State<myapp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
